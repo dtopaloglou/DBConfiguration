02b9c08f19347508aca55663044af9ae38edfea2
Dimitri Topaloglou <dtopaloglou@gmail.com>
2015-01-07 16:41:31 -0500

    Update DBConfig.php

--- DBConfig.php
+++ DBConfig.php
@@ -2,27 +2,9 @@
 class DBConfig {
 	
 	public static function read($key) {
+		$model = DatabaseConfigLoaderModel::getModel();
 		$key = preg_replace('/\s+/', '', $key);
-		$matches = array();
-		
-		foreach(DatabaseConfigLoaderModel::GetModel()->getModeledData() as $keyIndex => $value) {
-			if(preg_match('/^'.$key.'/', $keyIndex)) {
-				$matches[$keyIndex] = $value;
-				$matches = ConfigUtils::buildArray($matches);
-			}
-		}	
-		
-		if(!empty($matches)) {
-			$e =  array_filter(explode(DatabaseConfigLoaderModel::GetModel()->getDelimeter(), $key));
-			for($n = 0; $n < count($e) - 1; $n++) {
-				$matches = array_shift($matches);	
-			}
-			$matches = $matches[end($e)];
-		} else {
-			$matches = null;
-		}	
-
-		return $matches;
+		return new Setting($model, $key);
 		
 	}	
 }
