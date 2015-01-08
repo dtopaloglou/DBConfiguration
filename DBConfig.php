5b109a9f649be21261bbaba07108236727d1bb1c
Dimitri Topaloglou <dtopaloglou@gmail.com>
2015-01-07 22:53:49 -0500

    Update DBConfig.php

--- DBConfig.php
+++ DBConfig.php
@@ -4,7 +4,7 @@ class DBConfig {
 	public static function read($key) {
 		$model = DatabaseConfigLoaderModel::getModel();
 		$key = preg_replace('/\s+/', '', $key);
-		return new Setting($model, $key);
+		return new AppSetting($model, $key);
 		
 	}	
 }
